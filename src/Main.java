7aaeacb5a2a6e042d1cfee88d8d247013656264d
nursultanjuraev <nursultsnjuraev95@gmail.com>
2024-11-11 17:00:43 +0600

    fer

--- src/Main.java
+++ src/Main.java
@@ -10,11 +10,12 @@ public class Main {
 
         Person person1 = new Person("Aibek", "+996700700700", "kyrgyz", 19);
         Person person2 = new Person("Ernic", "+996700700700", "kyrgyz", 18);
+        Person person3 = new Person("Ernic", "+996700700700", "kyrgyz", 17);
 
         Student student1 = new Student("Aibek2", "+996700700700", "kyrgyz", 11);
         Student student2 = new Student("Aibek2", "+996700700700", "kyrgyz", 12);
 
-        Object[] objects = {person1, person2, student1, student2};
+        Object[] objects = {person1, person2,person3,student1, student2};
         for (Object object : objects) {
 
             System.out.println("object.toString() = " + object.toString());
